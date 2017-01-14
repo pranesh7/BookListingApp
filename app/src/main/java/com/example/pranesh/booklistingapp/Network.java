eab59d39f5596d0211cae921f4ed38010825a8a0
pranesh07 <praneshsgnhss@gmail.com>
2017-01-13 23:05:41 -0500

    Test file commit

---
+++ app/src/main/java/com/example/pranesh/booklistingapp/Network.java
@@ -0,0 +1,8 @@
+package com.example.pranesh.booklistingapp;
+
+/**
+ * Created by pranesh on 13/01/17.
+ */
+
+public class Network {
+}
--- app/src/main/java/com/example/pranesh/booklistingapp/QueryUtils.java
+++ app/src/main/java/com/example/pranesh/booklistingapp/QueryUtils.java
@@ -52,7 +52,7 @@ public final class QueryUtils {
             // If an error is thrown when executing any of the above statements in the "try" block,
             // catch the exception here, so the app doesn't crash. Print a log message
             // with the message from the exception.
-            Log.e("QueryUtils", "Problem parsing the earthquake JSON results", e);
+            Log.e("QueryUtils", "Problem parsing the BookListing JSON results", e);
         }
         // Return the list of earthquakes
         return books;
